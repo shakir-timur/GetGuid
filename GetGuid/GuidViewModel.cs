ff815a0aa4f7125c95a0884d10273a53febcb533
timur-sh <timur.shakirzyanov@micros.uz>
2021-09-10 09:51:21 +0500

    Добавьте файлы проекта.

---
+++ GetGuid/GuidViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace GetGuid
+{
+    public class GuidViewModel : INotifyPropertyChanged
+    {
+        public string GUID { get; set; } = Guid.NewGuid().ToString();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand CutToClipBoard { get; }
+
+        public ICommand Refresh { get; }
+
+        public GuidViewModel()
+        {
+            Refresh = new QuickCommand(() =>
+            {
+                this.GUID = Guid.NewGuid().ToString();
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(GUID)));
+            });
+
+            CutToClipBoard = new QuickCommand(() =>
+            {
+                Clipboard.SetData(DataFormats.Text, this.GUID);
+                this.GUID = "";
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(GUID)));
+            });
+        }
+    }
+}
---
+++ GetGuid/QuickCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace GetGuid
+{
+    class QuickCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        Action execute;
+        Func<bool> canExecute;
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute.Invoke();
+        }
+
+        public void Execute(object parameter)
+        {
+            execute.Invoke();
+        }
+
+        public QuickCommand(Action execute, Func<bool> canExecute = null)
+        {
+            this.canExecute = canExecute ?? (() => true);
+            this.execute = execute;
+        }
+    }
+}
